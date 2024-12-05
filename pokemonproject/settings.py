952a73d986ab13aacc2d5ad2514d87d584a8228f
syandbb <syandbb@syandbbs-MacBook-Air.local>
2024-12-04 19:24:38 -0500

    deploy

--- pokemonproject/settings.py
+++ pokemonproject/settings.py
@@ -29,13 +29,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#7lz%7l8k^6z2!h5qf_rov6)fyu-nb0f^tn@=a_y^l7)s^1wyv'
+SECRET_KEY = os.environ.get("SECRET_KEY", "localkey2024") #replace the SECRET_KEY VARIABLE with this. 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  'RENDER' not in os.environ  # replace the DEBUG variable with this. 
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -47,18 +50,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pokemon.apps.PokemonConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders', #add this below all the other apps
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', #add this!!!!!!
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#Below variable: MIDDLEWARES add this variable
+CORS_ALLOW_ALL_ORIGINS = True ## Variable CORS_ALLOW_ALL_ORIGINS is set to True to allow unrestricted access to the API.
 
 ROOT_URLCONF = 'pokemonproject.urls'
 
